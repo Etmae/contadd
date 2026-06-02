5a905aead8ba38d312024b63ba9bcb700a970be7
Your Name <you@example.com>
2026-06-02 12:13:33 +0100

    Cron Job Setup

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+# An ultra-lightweight ping function that executes in fractions of a millisecond
+def live_ping(request):
+    return HttpResponse("warm", content_type="text/plain")
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('live-ping/', live_ping, name='live_ping'),
     path('', include('contacts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
